49429db296503f74047d3181aa9e5acc322a11b3
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-04 14:18:50 +0900

    feat: idea domain에 entity 추가(#3)

---
+++ src/main/java/com/idear/backend/idea/domain/IdeaFile.java
@@ -0,0 +1,43 @@
+package com.idear.backend.idea.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class IdeaFile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long fileId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idea_id", nullable = false)
+	private Idea idea;
+
+	@Column(nullable = false)
+	private String fileName;
+
+	@Column(nullable = false)
+	private String filePath;
+
+	@Enumerated(EnumType.STRING)
+	private FileType fileType;
+
+	public enum FileType {
+		IMAGE,
+		ATTACHMENT
+	}
+}
