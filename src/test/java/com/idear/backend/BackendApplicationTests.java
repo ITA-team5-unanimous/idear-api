374ec7e1bf3af0e82dc97d53e13a579de887f1df
kisusu <kikisu115@naver.com>
2025-11-11 20:38:26 +0900

    test: CI 환경을 위한 테스트 설정 추가 (#7)
    - Embedded Redis 의존성 추가
    - 테스트용 application.yml 설정 추가

--- src/test/java/com/idear/backend/BackendApplicationTests.java
+++ src/test/java/com/idear/backend/BackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.idear.backend;
 
+import com.idear.backend.config.EmbeddedRedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(EmbeddedRedisConfig.class)
 class BackendApplicationTests {
 
 	@Test
