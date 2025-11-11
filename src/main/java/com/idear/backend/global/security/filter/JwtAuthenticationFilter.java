8ff2d871b408da7ae3c3d19a4d506f7eb7a575bc
kisusu <kikisu115@naver.com>
2025-11-11 18:59:18 +0900

    feat: Reissue 로직 구현 및 Security 설정 통합 (#7)
    - Reissue 로직 구현
    - JWT 인증 필터 추가
    - Security 예외 처리 핸들러 구현
    - Spring Security 설정 추가
    - 인증 테스트 엔드포인트 추가(/whoami)

---
+++ src/main/java/com/idear/backend/global/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.idear.backend.global.security.filter;
+
+import com.idear.backend.global.security.token.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String accessToken = tokenProvider.resolveToken(request);
+
+        if (accessToken != null) {
+            Authentication authentication = tokenProvider.validateToken(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
