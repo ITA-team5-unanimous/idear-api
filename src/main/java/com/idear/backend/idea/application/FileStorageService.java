6f81ab0fd17d6890c029275f90433e3a9019876d
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-12 19:33:30 +0900

    feat: adjust s3 file upload method(#3)

--- src/main/java/com/idear/backend/idea/application/FileStorageService.java
+++ src/main/java/com/idear/backend/idea/application/FileStorageService.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
-	String uploadFile(MultipartFile file, String uploadDir) throws IOException;
-	void deleteFile(String storedFileName, String uploadDir);
+	String uploadFile(MultipartFile file, String fileName, String uploadDir) throws IOException;
+	void deleteFile(String fileName, String uploadDir) ;
 }
