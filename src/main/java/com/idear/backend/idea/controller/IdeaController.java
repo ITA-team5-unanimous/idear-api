98a78cf91c6aecac7a9203f547899a8754856548
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-13 15:39:35 +0900

    feat: add getIdeaByUser api(controller)(#12)

--- src/main/java/com/idear/backend/idea/controller/IdeaController.java
+++ src/main/java/com/idear/backend/idea/controller/IdeaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.idear.backend.global.ApiResponse;
 import com.idear.backend.idea.application.IdeaService;
 import com.idear.backend.idea.dto.request.IdeaRegisterRequest;
+import com.idear.backend.idea.dto.response.IdeaResponse;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -47,6 +49,17 @@ public class IdeaController {
 	) {
 		ideaService.deleteIdea(ideaId);
 		return ResponseEntity.ok(ApiResponse.success());
+	}
+
+	/**
+	 * GET /api/idea/{useId} : 유저별 아이디어 조회
+	 */
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<IdeaResponse>>> getIdeasByUser(
+		@RequestParam("userId") Long userId
+	) {
+		List<IdeaResponse> ideas = ideaService.getIdeasByUser(userId);
+		return ResponseEntity.ok(ApiResponse.success(ideas));
 	}
 
 
