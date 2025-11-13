2f6e67b12ff1cbaa650ee3c60f5ba6e1a35cf808
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-13 14:12:14 +0900

    refactor: code 일부 개선사항 적용(#3)

--- src/main/java/com/idear/backend/idea/application/IdeaService.java
+++ src/main/java/com/idear/backend/idea/application/IdeaService.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.idear.backend.idea.domain.Idea;
 import com.idear.backend.idea.domain.IdeaFile;
 import com.idear.backend.idea.dto.request.IdeaRegisterRequest;
-import com.idear.backend.idea.infrastructure.repository.IdeaFileRepository;
 import com.idear.backend.idea.infrastructure.repository.IdeaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -22,24 +21,20 @@ import lombok.RequiredArgsConstructor;
 public class IdeaService {
 
 	private final IdeaRepository ideaRepository;
-	private final IdeaFileRepository ideaFileRepository;
 	private final FileStorageService fileStorageService;
 
 	@Transactional
 	public void registerIdea(IdeaRegisterRequest ideaRegisterRequest, List<MultipartFile> files) throws IOException {
 		Idea idea = Idea.registerIdea(
-			ideaRegisterRequest.getTitle(), ideaRegisterRequest.getShortDescription(), ideaRegisterRequest.getShortDescription()
+			ideaRegisterRequest.getTitle(), ideaRegisterRequest.getShortDescription(), ideaRegisterRequest.getDescription()
 		);
 		ideaRepository.save(idea);
 
 		List<IdeaFile> ideaFiles = new ArrayList<>();
 		if (files != null && !files.isEmpty()) {
-			for(MultipartFile i:files)
-				System.out.println(i.getOriginalFilename());
-
 			for (MultipartFile file : files) {
 				if (!file.isEmpty()) {
-					String fileName = String.valueOf(UUID.nameUUIDFromBytes(file.getName().getBytes()));
+					String fileName = UUID.randomUUID().toString();
 					String dir = parseExtension(file);
 					IdeaFile.FileType fileType = dir.equals("image") ? IdeaFile.FileType.IMAGE : IdeaFile.FileType.FILE;
 					String url = fileStorageService.uploadFile(file, fileName, dir);
@@ -51,7 +46,6 @@ public class IdeaService {
 
 		if(!ideaFiles.isEmpty()){
 			idea.addFile(ideaFiles);
-			ideaRepository.save(idea);
 		}
 
 		//TODO blockchain 등록, hash 저장, 완료 후 status 변경
