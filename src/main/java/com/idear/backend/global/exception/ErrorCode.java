c82cdba6ba8d67f0c0ae83aa914ab72a988ff0ad
yezanee <rbfrk02@naver.com>
2025-11-13 14:43:02 +0900

    chore: 충돌 해결

--- src/main/java/com/idear/backend/global/exception/ErrorCode.java
+++ src/main/java/com/idear/backend/global/exception/ErrorCode.java
@@ -28,14 +28,8 @@ public enum ErrorCode {
 
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 유저입니다."),
-    EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "C000", "잘못된 요청입니다."),
-
-    // 크롤링 관련 에러
-    CRAWLING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "크롤링 중 오류가 발생했습니다."),
-    PAGE_PARSING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "C002", "페이지 파싱에 실패했습니다."),
-    CONTEST_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "공모전 저장에 실패했습니다.");
-
-
+    ;
+    
     private final HttpStatus status;
     private final String code;
     private final String message;
