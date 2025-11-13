5b12e62116d20dbff6d6e79e174f10176d0c4ed8
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-11-13 15:58:44 +0900

    feat: add user column to Idea domain(#12)

--- src/main/java/com/idear/backend/idea/domain/Idea.java
+++ src/main/java/com/idear/backend/idea/domain/Idea.java
@@ -4,15 +4,20 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.idear.backend.user.domain.User;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,11 +31,12 @@ public class Idea {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ideaId;
 
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
 	//TODO 도메인 연관관계 설정
-	// @ManyToOne(fetch = FetchType.LAZY)
-	// @JoinColumn(name = "member_id", nullable = false)
-	// private Member member;
-	//
 	// @ManyToOne(fetch = FetchType.LAZY)
 	// @JoinColumn(name = "contest_id")
 	// private Contest contest;
@@ -61,7 +67,8 @@ public class Idea {
 		WITHDRAWN
 	}
 
-	private Idea(String title, String shortDescription, String description, IdeaStatus status, LocalDateTime createdAt) {
+	private Idea(User user, String title, String shortDescription, String description, IdeaStatus status, LocalDateTime createdAt) {
+		this.user = user;
 		this.title = title;
 		this.shortDescription = shortDescription;
 		this.description = description;
@@ -69,8 +76,8 @@ public class Idea {
 		this.createdAt = createdAt;
 	}
 
-	public static Idea registerIdea(String title, String shortDescription, String description){
-		return new Idea(title, shortDescription, description, IdeaStatus.PROVING, LocalDateTime.now());
+	public static Idea registerIdea(User user, String title, String shortDescription, String description){
+		return new Idea(user, title, shortDescription, description, IdeaStatus.PROVING, LocalDateTime.now());
 	}
 
 	public void addFile(List<IdeaFile> files){
