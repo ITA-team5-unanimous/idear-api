49628c302b34fc46ac02de45f4454619fd2bb302
sohyeonjung <jhyeon23@konkuk.ac.kr>
2026-01-12 21:51:35 +0900

    docs: swagger 설정 추가(#51)

--- src/main/java/com/idear/backend/inquiry/controller/InquiryController.java
+++ src/main/java/com/idear/backend/inquiry/controller/InquiryController.java
@@ -6,6 +6,7 @@ import com.idear.backend.inquiry.application.service.InquiryService;
 import com.idear.backend.inquiry.dto.InquiryCreateRequest;
 import com.idear.backend.user.domain.User;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +25,12 @@ public class InquiryController {
 
     private final InquiryService inquiryService;
 
-    @PostMapping
+    @Operation(summary = "문의 생성", description = "사용자 문의를 생성합니다. 문의 내용과 함께 최대 4장의 에러 이미지를 첨부할 수 있습니다.")
+    @PostMapping(consumes = { "multipart/form-data" })
     public ResponseEntity<ApiResponse> createInquiry(
             @Parameter(hidden = true) @ValidatedUser User user,
-            @RequestPart("request") InquiryCreateRequest inquiryCreateRequest,
-            @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+            @Parameter(description = "문의 내용", required = true) @RequestPart("request") InquiryCreateRequest inquiryCreateRequest,
+            @Parameter(description = "에러 이미지 파일 (최대 4장)") @RequestPart(value = "images", required = false) List<MultipartFile> images) {
         inquiryService.createInquiry(user, inquiryCreateRequest, images);
         return ResponseEntity.ok(ApiResponse.success());
     }
